9b22a224664d3b15d87cce756fa2255041ea04a4
limichange <limichange@hotmail.com>
2017-02-28 15:42:58 +0800

    🎨  - Improving structure / format of the code.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('./easeljs-0.8.2.combined.js')
-require('./preloadjs-0.6.2.combined.js')
-require('./soundjs-0.6.2.combined.js')
-require('./tweenjs-0.6.2.combined.js')
+require('./lib/easeljs-0.8.2.combined.js')
+require('./lib/preloadjs-0.6.2.combined.js')
+require('./lib/soundjs-0.6.2.combined.js')
+require('./lib/tweenjs-0.6.2.combined.js')
